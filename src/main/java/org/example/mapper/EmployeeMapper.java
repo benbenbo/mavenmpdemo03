2edb5753d182cc8e1b80984fa292dca5423af69f
XBird <zbyfoshan@163.com>
2021-04-21 22:29:20 +0800

    实体类-插件自动生成

---
+++ src/main/java/org/example/mapper/EmployeeMapper.java
@@ -0,0 +1,16 @@
+package org.example.mapper;
+
+import org.example.beans.Employee;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zhengbingyuan
+ * @since 2021-04-17
+ */
+public interface EmployeeMapper extends BaseMapper<Employee> {
+
+}
---
+++ src/main/java/org/example/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package org.example.service;
+
+import org.example.beans.Employee;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zhengbingyuan
+ * @since 2021-04-17
+ */
+public interface EmployeeService extends IService<Employee> {
+
+}
